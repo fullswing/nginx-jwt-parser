fd7f166c2e8c5ec8bbddf367cf07a8cd28a110c3
fullswing0603 <fullswing0603@gmail.com>
2022-06-02 22:06:38 +0900

    init

---
+++ jwt.js
@@ -0,0 +1,14 @@
+function decode_jwt(data) {
+  var parts = data
+    .split(".")
+    .slice(0, 2)
+    .map((v) => Buffer.from(v, "base64url").toString())
+    .map(JSON.parse);
+  return { headers: parts[0], payload: parts[1] };
+}
+
+function get_user_email(r) {
+  return decode_jwt(r.headersIn.Authorization.slice(7)).payload.email;
+}
+
+export default { get_user_email };
